231922e3f6c4d5362c3c3d63a96abc86a88f16d0
Alec Tower <alectower@gmail.com>
2015-03-06 16:20:31 -0500

    Move front end alerts to root path

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,1 +1,3 @@
+import DS from 'ember-data';
+
 export default DS.ActiveModelAdapter.extend();
--- app/router.js
+++ app/router.js
@@ -6,9 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('alerts', function() {
-    this.route('new');
-  });
+  this.resource('alerts', { path: '/' });
 });
 
 export default Router;
