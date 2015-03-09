f4d174ca969043a6323d30f6132dda118e3a0f86
Alec Tower <alectower@gmail.com>
2015-03-09 18:36:25 -0400

    Add basic email login

--- app/authenticators/custom.js
+++ app/authenticators/custom.js
@@ -1,10 +1,28 @@
-//import Base from 'simple-auth/authenticators/base';
-//
-//export default Base.extend({
-//  restore: function(data) {
-//  },
-//  authenticate: function(options) {
-//  },
-//  invalidate: function(data) {
-//  }
-//});
+import Ember from 'ember';
+import Base from 'simple-auth/authenticators/base';
+
+export default Base.extend({
+  restore: function(data) {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      if (!Ember.isEmpty(data.userEmail)) {
+        resolve(data);
+      } else {
+        reject();
+      }
+    });
+  },
+
+  authenticate: function(options) {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.run(function() {
+        resolve({ userEmail: options.identification });
+      });
+    })
+  },
+
+  invalidate: function(data) {
+    return new Ember.RSVP.Promise(function(resolve) {
+      resolve({ userEmail: null });
+    })
+  }
+});
--- app/routes/alerts.js
+++ app/routes/alerts.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
-    return this.store.find('alert', { email: 'alectower@gmail.com' });
+    return this.store.find('alert', { email: this.get('session').get('user_email') });
   }
 });
